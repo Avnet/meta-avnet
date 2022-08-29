5e3492ee7735ae23a8ecbc282f8164825571eae1
Thomas Nizan <tnizan@witekio.com>
2022-08-29 16:19:18 +0000

    recipes-apps: factest: refactor main script and add colors

--- recipes-apps/factest/files/zub1cg-sbc/factest.sh
+++ recipes-apps/factest/files/zub1cg-sbc/factest.sh
@@ -9,21 +9,53 @@ source $FACTEST_SCRIPTS_DIR/qspi_utils.sh
 nb_passed=0
 nb_failed=0
 
+cecho(){
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"  # <-- [0 means not bold
+    YELLOW="\033[1;33m" # <-- [1 means bold
+    CYAN="\033[1;36m"
+    # ... Add more colors if you like
+
+    NC="\033[0m" # No Color
+
+    # printf "${(P)1}${2} ${NC}\n" # <-- zsh
+    printf "${!1}${2} ${NC}\n" # <-- bash
+}
+execute_command_test () {
+   desc=$1
+   command=$2
+
+   cecho "CYAN" "\n--------- $desc ---------\n"
+   $command
+   status=$?
+
+   if [ $status -ne 0 ]
+   then
+      nb_failed=$((nb_failed+1))
+      cecho "RED" "\n--------- $desc - FAIL ---------"
+   else
+      nb_passed=$((nb_passed+1))
+      cecho "GREEN" "\n--------- $desc - PASS ---------"
+   fi
+
+   return $status
+}
+
 execute_script_test () {
    desc=$1
    script=$2
 
-   echo -e "\n--------- $desc ---------\n"
+   cecho "CYAN" "\n--------- $desc ---------\n"
    /bin/sh $script
    status=$?
 
    if [ $status -ne 0 ]
    then
       nb_failed=$((nb_failed+1))
-      echo -e "\n--------- $desc - FAIL ---------"
+      cecho "RED" "\n--------- $desc - FAIL ---------"
    else
       nb_passed=$((nb_passed+1))
-      echo -e "\n--------- $desc - PASS ---------"
+      cecho "GREEN" "\n--------- $desc - PASS ---------"
    fi
 
    return $status
@@ -33,26 +65,19 @@ print_results () {
    nb_test=$((nb_passed + nb_failed))
       echo -e "\n\n-----------------------------------"
       echo -e "------------- RESULTS -------------"
-      echo -e " Ran:    $nb_test tests"
-      echo -e " Passed: $nb_passed tests"
-      echo -e " Failed: $nb_failed tests"
+      cecho "CYAN" " Ran:    $nb_test tests"
+      cecho "GREEN" " Passed: $nb_passed tests"
+      cecho "RED" " Failed: $nb_failed tests"
       echo -e "-----------------------------------\n"
 }
 
 
 {
-   # QSPI TEST:
    QSPI_STATUS=0
-   echo -e "\n--------- QSPI TEST (mount user partition) ---------\n"
-   qspi_mount_user_part
-   if [ $? -ne 0 ]
+   execute_command_test "QSPI TEST (mount user partition)" "qspi_mount_user_part"
+   if [[ $? -ne 0 ]]
    then
-      nb_failed=$((nb_failed+1))
-      echo -e "\n--------- QSPI TEST - FAIL ---------"
       QSPI_STATUS=1
-   else
-      nb_passed=$((nb_passed+1))
-      echo -e "\n--------- QSPI TEST - PASS ---------"
    fi
 
    execute_script_test "Test Buttons and LEDs" $FACTEST_SCRIPTS_DIR/buttons_and_leds_test.sh
