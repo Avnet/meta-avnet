dda22a080343a660f4e5810a2571b05d2d243dab
Bruce Thomson <bthomson@witekio.com>
2022-09-23 12:52:32 +0100

    Add basic unattended mode for emi test to run on boot

--- recipes-apps/factest/files/zub1cg-sbc/factest.sh
+++ recipes-apps/factest/files/zub1cg-sbc/factest.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-FACTEST_SCRIPTS_DIR=/home/root/factest_scripts
+# Check if there are any switches such unattended mode
+unattended=0
 
+if [[ $# -gt 0 ]]
+then
+      for var in "$@"
+      do
+         if [[ $var -eq "-u" ]]
+         then
+            unattended=1
+         fi
+      done
+fi
+
+FACTEST_SCRIPTS_DIR=/home/root/factest_scripts
 LOCAL_RESULTS_FILE=last_factest_results.log
+
+# In unattended mode we don't write the results to local file system
+# to prevent prevent this from filling up the disk
+if [[ $unattended -eq 1 ]]
+then
+   LOCAL_RESULTS_FILE=/dev/null
+fi
+
+
 QSPI_RESULTS_FILE=factest_results.log
 source $FACTEST_SCRIPTS_DIR/qspi_utils.sh
 
@@ -46,7 +68,13 @@ execute_script_test () {
    script=$2
 
    cecho "CYAN" "\n--------- $desc ---------\n"
-   /bin/sh $script
+   if [[ $unattended -eq 1 ]]
+   then
+      /bin/sh $script -u
+   else
+      /bin/sh $script
+   fi
+
    status=$?
 
    if [ $status -ne 0 ]
@@ -73,12 +101,19 @@ print_results () {
 
 
 {
-   QSPI_STATUS=0
-   execute_command_test "QSPI TEST (mount user partition)" "qspi_mount_user_part"
-   if [[ $? -ne 0 ]]
+   if [[ $unattended -eq 1 ]]
    then
-      QSPI_STATUS=1
+      # In unattended mode, we don't write the results to the QSPI
+      cecho "CYAN" "\n--------- Running test suite in unattended mode ---------\n"
+   else
+      QSPI_STATUS=0
+      execute_command_test "QSPI TEST (mount user partition)" "qspi_mount_user_part"
+      if [[ $? -ne 0 ]]
+      then
+         QSPI_STATUS=1
+      fi
    fi
+
 
    execute_script_test "Test Buttons and LEDs" $FACTEST_SCRIPTS_DIR/buttons_and_leds_test.sh
    execute_script_test "Test Click Module interface" $FACTEST_SCRIPTS_DIR/click_test.sh
@@ -90,14 +125,18 @@ print_results () {
    execute_script_test "Test RTC" $FACTEST_SCRIPTS_DIR/rtc_test.sh
 
    print_results
-} > >(tee "$LOCAL_RESULTS_FILE") 2>&1
+} > >(tee -a "$LOCAL_RESULTS_FILE") 2>&1
 
 sync
 
-if [[ $QSPI_STATUS -eq 0 ]]
+# Only copy the results to the QSPI if test is running in unattended mode
+if [[ $unattended -eq 0 ]]
 then
-   echo "Copying Test Results to QSPI"
-   copy_log_file_to_qspi $LOCAL_RESULTS_FILE $QSPI_RESULTS_FILE
-else
-   echo "QSPI TEST Failed, log file can be found on the sd card: /home/root/$LOCAL_RESULTS_FILE"
+   if [[ $QSPI_STATUS -eq 0 ]]
+   then
+      echo "Copying Test Results to QSPI"
+      copy_log_file_to_qspi $LOCAL_RESULTS_FILE $QSPI_RESULTS_FILE
+   else
+      echo "QSPI TEST Failed, log file can be found on the sd card: /home/root/$LOCAL_RESULTS_FILE"
+   fi
 fi
